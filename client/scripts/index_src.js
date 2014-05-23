81732479d66567316772612e1c1c61714e2aa024
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-05-23 13:39:13 -0400

    fixed for npm

--- client/scripts/index_src.js
+++ client/scripts/index_src.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var manuscript = require('../../lib/manuscript'),
-	manuscriptapp = new manuscript({
-		idSelector : 'template'
-	});
+// var manuscript = require('../../lib/manuscript'),
+// 	manuscriptapp = new manuscript({
+// 		idSelector : 'template'
+// 	});
 
-window.onload = function(){
-	manuscriptapp.init();
-};
+// window.onload = function(){
+// 	manuscriptapp.init();
+// };
 
-manuscriptapp.on("intializedManuscript",function(data){
-	console.log("loaded manuscript",data);
-});
+// manuscriptapp.on("intializedManuscript",function(data){
+// 	console.log("loaded manuscript",data);
+// });
 
-window.manuscriptapp = manuscriptapp;
+// window.manuscriptapp = manuscriptapp;
